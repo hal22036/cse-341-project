5b5a00818b4dd330532bad2ba4d0549662091be8
Jeni Hales <hal22036@byui.edu>
2023-09-14 12:41:31 -0700

    updated home command

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 
 // router.get('/', (req, res) => {res.send('Hello World');});
 
-router.use('/contacts', require('./contacts'));
+router.use('/', require('./contacts'));
 
 module.exports = router;
