12ee29077fda7f2bb3bee9c3b1550e4802c87f25
Jeni Hales <hal22036@byui.edu>
2023-09-20 08:56:20 -0700

    swagger set-up

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -3,6 +3,7 @@ const mongodb = require('../data/database');
 const objectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
+    //#swagger.tags=['Contacts']
     const result = await mongodb.getDatabase().db().collection('contacts').find();
     result.toArray().then((contacts) => {
         res.setHeader('Content-Type', 'application/json');
@@ -11,15 +12,65 @@ const getAll = async (req, res) => {
 };
 
 const getSingle = async (req, res) => {
-    const userId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().db().collection('contacts').find({_id: userId});
+    //#swagger.tags=['Contacts']
+    const contactId = new ObjectId(req.params.id);
+    const result = await mongodb.getDatabase().db().collection('contacts').find({_id: contactId});
     result.toArray().then((contacts) => {
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(contacts[0]);
     });
 };
 
+const createContact = async (req, res) => {
+    //#swagger.tags=['Contacts']
+    const contact = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    };
+    const response = await mongodb.getDatabase().db().collection('contacts').insertOne(contact);
+    if (response.acknowledged) {
+        res.status(204).send();
+    } else{
+        res.status(500).json(response.error || 'Some error occured while updating the contact.');
+    }
+};
+
+const updateContact = async (req, res) => {
+    //#swagger.tags=['Contacts']
+    const contactId = new ObjectId(req.params.id);
+    const contact = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    };
+    const response = await mongodb.getDatabase().db().collection('contacts').replaceOne({ _id: contactId }, contact);
+    if (response.modifiedCount) {
+        res.status(204).send();
+    } else{
+        res.status(500).json(response.error || 'Some error occured while updating the contact.');
+    }
+};
+
+const deleteContact = async (req, res) => {
+    //#swagger.tags=['Contacts']
+    const contactId = new ObjectId(req.params.id);
+    const response = await mongodb.getDatabase().db().collection('contacts').deleteOne({ _id: contactId });
+    if (response.deleteCount > 0) {
+        res.status(204).send();
+    } else{
+        res.status(500).json(response.error || 'Some error occured while updating the contact.');
+    }
+};
+
 module.exports = {
     getAll,
-    getSingle
+    getSingle,
+    createContact,
+    updateContact,
+    deleteContact
 };
