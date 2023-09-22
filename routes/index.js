948035ec832857843738843bbce52579181b875b
Jeni Hales <hal22036@byui.edu>
2023-09-22 15:59:29 -0700

    update name

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,8 @@ const router = require('express').Router();
 router.use('/', require('./swagger'));
 
 router.get('/', (req, res) => {
-    //swagger.tags=['Hello World']
-    res.send('Hello World');
+    //swagger.tags=['Jeni Hales']
+    res.send('Jeni Hales');
 });
 
 router.use('/contacts', require('./contacts'));
